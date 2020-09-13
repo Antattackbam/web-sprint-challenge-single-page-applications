dd4b2e4c474c1ab1231b67bac8731fe049437331
Anthony Rodriguez <antcreatesmagic@gmail.com>
2020-09-13 13:00:00 -0500

    basic form

--- src/component/Form.js
+++ src/component/Form.js
@@ -2,11 +2,65 @@ import React from 'react';
 
 const Form  = props => {
 
-    return (
-        <div>
-            <h3>Build a pizza</h3>
-        </div>
-    )
-}
+    return ( 
+    <>
+    <h2>Build Your Own Pizza</h2>
+        <form >
+            <h3>Bulid Your Own Pizza</h3>
+            <label>Name:&nbsp;
+                <input
+                    type='text'
+                    placeholder='name'
+                    maxLength='30'
+                    name='first_name'
+                />
+            </label>  
+            <label>Size:&nbsp;
+                <select name='size'>
+                    <option> Select a Size</option>
+                    <option>Small</option>
+                    <option>Medium</option>
+                    <option>Large</option>
+                </select>
+            </label>
+            <div>
+                <label>Pepperoni&nbsp;
+                    <input
+                        type='checkbox'
+                        name='pepperoni'
+                    />
+                </label>
+                <label>Chicken&nbsp;
+                    <input
+                        type='checkbox'
+                        name='chicken'
+                    />
+                </label>
+                <label>Onions&nbsp;
+                    <input
+                        type='checkbox'
+                        name='onions'
+                    />
+                </label>
+                <label>Tomatoes&nbsp;
+                    <input
+                        type='checkbox'
+                        name='tomatoes'
+                    />
+                </label>
+            </div>
+            <label>Special Instructions:&nbsp;
+                <input
+                    type='text'
+                    placeholder='instructions'
+                    maxLength='200'
+                    name='instructions'
+                />
+            </label> 
+            <button>Submit</button>
+        </form>
+    </>
 
-export default Form;
+)
+}
+export default Form
