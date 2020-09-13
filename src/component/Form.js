2636066cc300ceb2f2e169986d39d1a7ab332d74
Anthony Rodriguez <antcreatesmagic@gmail.com>
2020-09-13 12:43:31 -0500

    set up frame/render components

---
+++ src/component/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Form  = props => {
+
+    return (
+        <div>
+            <h3>Build a pizza</h3>
+        </div>
+    )
+}
+
+export default Form;
---
+++ src/component/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = ()=> {
+    return (
+        <div>
+        <h2> Welcome to Pizza Palace!</h2>
+        <p>Get started on your order using the form below.</p>
+        </div>
+    )
+}
+
+export default Home;
